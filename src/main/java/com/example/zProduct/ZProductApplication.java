23efb881a1e6d715fa8bfc7652992a014ba6871c
SurajKoli9011 <surajkoli9011@gmail.com>
2023-12-01 10:12:32 +0530

    first commit

---
+++ src/main/java/com/example/zProduct/ZProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.zProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZProductApplication.class, args);
+	}
+
+}
